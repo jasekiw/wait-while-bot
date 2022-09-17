a3468e23c40781687e0fe6846157f33152a6d273
Jason Gallavin <jasongallavin@gmail.com>
2022-09-17 08:32:45 -0400

    Fix: waitForTime not working when tomorrow set
    getTargetDate needs to be evaluated only once, evaluating it multiple times will always get tomorrow and never elapse.

--- waitForTime.js
+++ waitForTime.js
@@ -8,18 +8,28 @@ const wait = require('./wait.js').wait;
  * @returns {Promise<void>}
  */
 async function waitForTime(tomorrow, hour, minutes) {
-  let isIt8 = false;
-  while(!isIt8) {
+  let isItTime = false;
+  const target = getTargetDate(tomorrow, hour, minutes);
+  while(!isItTime) {
     let currentTime = new Date();
-    let target = new Date();
-    target.setHours(hour, minutes, 0, 0);
-    target.setDate(target.getDate() + (tomorrow ? 1 : 0))
     let elapsed = (target - currentTime) / 1000
-    isIt8 = elapsed <= 0;
-    if(!isIt8) {
+    isItTime = elapsed <= 0;
+    if(!isItTime) {
       await wait(1)
     }
   }
+}
+
+/**
+ * @param {boolean} tomorrow
+ * @param {number} hour
+ * @param {number} minutes
+ */
+function getTargetDate(tomorrow, hour, minutes) {
+  let target = new Date();
+  target.setHours(hour, minutes, 0, 0);
+  target.setDate(target.getDate() + (tomorrow ? 1 : 0))
+  return target;
 }
 
 exports.waitForTime = waitForTime;
